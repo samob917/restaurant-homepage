e541a4515e5255ee5a2881afae35194a08a817fb
Sam Oberly <samob917@outlook.com>
2025-04-02 21:17:48 -0400

    Move homepage creation into index
    All the declaration of the layout is in the index page. The page works
    and looks the same.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, World.')
+import resImage from "./jay-wennington-restuarant.jpg";
+
+const page = document.querySelector("#content");
+
+const heading = document.createElement("h1");
+heading.textContent = "The best restuarant ever";
+page.appendChild(heading);
+
+const image = document.createElement("img");
+image.src = resImage;
+image.style.height = "50vh";
+image.style.width = "40vw";
+page.appendChild(image);
+
+const para = document.createElement("p");
+para.textContent = "We have a great selection of food. You will love it so much.";
+page.appendChild(para);
